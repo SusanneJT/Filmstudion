b2d5d38ecb0afc9a542b2dbb90384ac5cffbb65c
Susanne Trygg <susanne.trygg@gmail.com>
2020-01-31 13:10:19 +0100

    models created

---
+++ src/Filmstudion/Data/Repository/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Filmstudion.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmstudion.Data.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<FilmStudio> FilmStudios { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        //public DbSet<Lending> Lendings { get; set; }
+        //public DbSet<Rating> Ratings { get; set; }
+        //public DbSet<Trivia> Trivia { get; set; }
+    }
+}
--- src/Filmstudion/Startup.cs
+++ src/Filmstudion/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Filmstudion.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,12 @@ namespace Filmstudion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IFilmStudioRepository, FilmStudioRepository>();
+            //services.AddScoped<IFilmStudioRepository, FilmStudioRepository>();
+
             services.AddControllers();
         }
 
@@ -37,6 +45,7 @@ namespace Filmstudion
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
