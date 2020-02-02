8b2635ca86049f086d4a0510d9dc179e711c91cd
Susanne Trygg <susanne.trygg@gmail.com>
2020-02-02 02:49:39 +0100

    js test classes added

--- src/Filmstudion/wwwroot/js/script.js
+++ src/Filmstudion/wwwroot/js/script.js
@@ -4,22 +4,23 @@ if (sessionStorage.getItem("ChosenStudio") != null) {
     $("#dropdownMenuButton").html(sessionStorage.getItem("ChosenStudio"));
 
 }
-   
 
-const request = async () => {
-    //this.URL = "https://localhost:44393/api/filmstudios";
-    return fetch("https://localhost:44393/api/filmstudios").then(response => response.json());
-}
-
+filmstudio = new FilmStudio();
+movie = new Movie();
 
 $(document).ready(async function () {
-    var studios = await request();
+    AllStudios();
+    AllMovies();
+});
+
+async function AllStudios() {
+    var studios = await filmstudio.GetAll();
     $.each(studios, function (i) {
         $('<a class="dropdown-item" href="#" onclick="ChooseFilmStudio(\''
             + studios[i].studioName + '\')">' + studios[i].studioName + '</a > ').appendTo("#dropDownItems");
     });
-        
-});
+}
+
 
 function ChooseFilmStudio(studioName) {
     console.log(studioName);
@@ -27,5 +28,25 @@ function ChooseFilmStudio(studioName) {
     $("#dropdownMenuButton").html(studioName);
     $("#WarningBox").hide();
 }
+
+async function AllMovies() {
+    var movies = await movie.GetAll();
+    $.each(movies, function (i) {
+
+        $('<div class="mt-3 p-3"><div class="card h-100" style="width: 15rem;">'
+            + '<img class= "card-img-top" src="/img/image-not-found.png" alt="Card image cap">'
+            + '<div class="card-body d-flex flex-column">'
+            + '<h5 class="card-title">' + movies[i].movieTitle + '</h5>'
+            +  '<a href="#" class="btn btn-primary btn-sm mt-auto">Mer info</a>'
+            + '</div>'
+            + '</div></div>').appendTo("#MovieBox");
+
+        console.log(movies[i].movieTitle);
+        //$('<a class="dropdown-item" href="#" onclick="ChooseFilmStudio(\''
+            //+ studios[i].studioName + '\')">' + studios[i].studioName + '</a > ').appendTo("#dropDownItems");
+    });
+}
+
+
 
     
